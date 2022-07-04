18bf758fa5de5f393d321889d3ae379e2798199f
KuchumovaAnna <ap199173@gmail.com>
2022-07-04 12:37:15 +0300

    22

--- DZ11.java
+++ DZ11.java
@@ -1,11 +1,13 @@
 // Есть отсортированный массив, нужно используя минимальное количество попыток найти загаданное число
+package Java_seminar;
 
 import java.util.Arrays;
+
 public class DZ11 {
 
     public static void main(String[]args) {
         int[]SortArr=[1,2,3,4,5,6,7];
-        int key=Arrays.binarySearch(SortArr, key: 7);
+        int key=Arrays.binarySearch(SortArr, key);
         System.out.println(key);
     }
 }
