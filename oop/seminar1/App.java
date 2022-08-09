57b42f707201b66de62a4d8f4b2631f986c55367
KuchumovaAnna <ap199173@gmail.com>
2022-08-09 20:22:57 +0300

    oop dz1

--- oop/seminar1/App.java
+++ oop/seminar1/App.java
@@ -6,13 +6,19 @@ public class App {
         Person person = new Person("Dima", "V");
         Person person1 = new Person("Oleg", "V");
         Person person2 = new Person("Olga", "V");
+        Person person3 = new Person("Irina", "V");
+        Person person4 = new Person("Anna", "G");
+        Person person5 = new Person("Nikolay", "G");
 
         Tree tree = new Tree();
         tree.addLink(person1, person, Relation.PARENT, Relation.CHILD);
         tree.addLink(person1, person2, Relation.PARENT, Relation.CHILD);
+        tree.addLink(person3, person, Relation.PARENT, Relation.CHILD);
+        tree.addLink(person3, person2, Relation.PARENT, Relation.CHILD);
+        tree.addLink(person3, person5, Relation.PARENT, Relation.CHILD);
+        
 
-
-        Reserch.printAllChildren("Oleg", tree);
+        Reserch.printParent("Olga", tree);
     }
 
 }
--- oop/seminar1/Reserch.java
+++ oop/seminar1/Reserch.java
@@ -3,12 +3,12 @@ package Java_seminar.oop.seminar1;
 import java.util.List;
 
 public class Reserch {
-    public static void printAllChildren(String name, Tree tree) {
+    public static void printParent(String name, Tree tree) {
         List<Link> links = tree.getLinks();
 
         for (Link el : links) {
             if (el.getPersonFirst().getName().equals(name) &&
-            el.getLink() == Relation.PARENT) {
+            el.getLink() == Relation.CHILD) {
                 System.out.println(el.getPersonSecond());
             }
         }
