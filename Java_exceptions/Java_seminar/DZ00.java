dc738c5c6c591a26370f9ff462bcf51dc2df68c2
KuchumovaAnna <ap199173@gmail.com>
2022-11-18 13:49:20 +0300

    12356

---
+++ Java_exceptions/Java_seminar/DZ00.java
@@ -0,0 +1,12 @@
+package Java_seminar;
+
+public static void main(String[] args) 
+{
+    method1();
+}
+
+public static void method1() throws  FileNotFoundException, ClassNotFoundException
+{
+    //тут кинется исключение  FileNotFoundException, такого файла нет
+    FileInputStream fis = new FileInputStream("C2:\\badFileName.txt");
+}
