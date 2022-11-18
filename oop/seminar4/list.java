b5ff4f90b0576e4d12719a836f01b715ee0184fc
KuchumovaAnna <ap199173@gmail.com>
2022-11-18 13:51:10 +0300

    1235

--- oop/seminar4/list.java
+++ oop/seminar4/list.java
@@ -1,5 +1,12 @@
 package oop.seminar4;
 
-public class list {
-    
+public static void main(String[] args)
+{
+    method1();
+}
+
+public static void method1() throws  FileNotFoundException, ClassNotFoundException
+{
+    //тут кинется исключение  FileNotFoundException, такого файла нет
+    FileInputStream fis = new FileInputStream("C2:\\badFileName.txt");
 }
